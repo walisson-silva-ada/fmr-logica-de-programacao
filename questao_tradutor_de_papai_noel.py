6933193c4a57685101710a9b50c6cecb8489c938
Matheus Outi <matheusgoya.mg@gmail.com>
2022-01-06 23:08:01 -0300

    questao_tradutor_de_papai_noel.py adicionado

---
+++ questao_tradutor_de_papai_noel.py
@@ -0,0 +1,13 @@
+dicionario_pais_frase = {'brasil':'Feliz Natal!', 'alemanha':'Frohliche Weihnachten!', 'austria':'Frohe Weihnacht!',
+'coreia':'Chuk Sung Tan!','espanha':'Feliz Navidad!', 'grecia':'Kala Christougena!', 'estados-unidos':'Merry Christmas!',
+'inglaterra':'Merry Christmas!','australia':'Merry Christmas!', 'portugal':'Feliz Natal!', 'suecia':'God Jul!',
+'turquia':'Mutlu Noeller','argentina':'Feliz Navidad!','chile':'Feliz Navidad!','mexico':'Feliz Navidad!',
+'antardida':'Merry Christmas!','canada':'Merry Christmas!','irlanda':'Nollaig Shona Dhuit!',
+'belgica': 'Zalig Kerstfeest!', 'italia':'Buon Natale!', 'libia':'Buon Natale!','siria':'Milad Mubarak!',
+'marrocos':'Milad Mubarak!','japao':'Merii Kurisumasu!'}
+
+pais_atual = input('Digite o nome do país: ').strip().lower()
+if pais_atual not in dicionario_pais_frase.keys():
+    print('--- NOT FOUND ---')
+else:
+    print(dicionario_pais_frase[pais_atual])
